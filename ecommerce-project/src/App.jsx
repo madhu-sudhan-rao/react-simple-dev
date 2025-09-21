69b8ebf239d31b2ae586f0364eab9f93bf1fc7f0
Madhu Sudhan Rao <madhusudanrao.pediredla@imaginnovate.com>
2025-09-21 13:23:54 +0530

    add Header component and integrate it into Home, Orders, and Tracking pages; create TrackingPage styles

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import Checkout from "./pages/Checkout";
 import OrdersPage from "./pages/OrdersPage";
+import TrackingPage from "./pages/TrackingPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="checkout" element={<Checkout />} />
         <Route path="orders" element={<OrdersPage />} />
+        <Route path="tracking" element={<TrackingPage />} />
       </Routes>
     
     </>
