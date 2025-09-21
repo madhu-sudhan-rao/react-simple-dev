356377a6d5966f7834375c6e232c6cc1ccc10437
Madhu Sudhan Rao <madhusudanrao.pediredla@imaginnovate.com>
2025-09-21 13:15:10 +0530

    update link to other pages to match the router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import Checkout from "./pages/Checkout";
 
 function App() {
   return (
     <>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path="checkout" element={<HomePage />} />
+        <Route path="checkout" element={<Checkout />} />
       </Routes>
     
     </>
