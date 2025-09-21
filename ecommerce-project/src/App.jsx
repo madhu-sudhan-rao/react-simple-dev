edad75329ea7431b701f600d61dca1b4e456a80d
Madhu Sudhan Rao <madhusudanrao.pediredla@imaginnovate.com>
2025-09-21 13:08:17 +0530

    create multiple pages using react-router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,14 +1,17 @@
-import './App.css'
-import HomePage from './pages/HomePage'
+import { Routes, Route } from "react-router";
+import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
-
   return (
     <>
-    <HomePage />
-     
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<HomePage />} />
+      </Routes>
+    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
